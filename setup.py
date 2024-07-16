9b9fc01ce68aeff2ace9ca9bb88532bbabc5f102
MarieStLaurent <marie.stl@gmail.com>
2024-07-16 12:54:26 -0400

    [DATALAD] Recorded changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Vision-Language-Brain Model adapted from OpenVLA for the CNeuroMod Phantom project',
+    author='CNeuroMod',
+    license='MIT',
+)
