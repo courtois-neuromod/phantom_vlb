fb8fd77d46468e6dda5b819f5f2ba6ad3a403cc4
MarieStLaurent <marie.stl@gmail.com>
2025-04-11 17:59:38 -0400

    logger dev

--- train_dev_tests.py
+++ train_dev_tests.py
@@ -1,5 +1,6 @@
 """."""
 
+import logging
 import os
 import subprocess
 from pathlib import Path
@@ -36,6 +37,8 @@ def train(config: DictConfig) -> None:
         The validation loss.
     """
     # Debugging sanity checks
+    logging.info(config)
+
     print(OmegaConf.to_yaml(config))
     print(config.subject, config.random_state)
     print(config.datamodule.config.subject, config.datamodule.config.random_state)
@@ -47,10 +50,11 @@ def train(config: DictConfig) -> None:
     os.environ["TRANSFORMERS_CACHE"] = config.cache_dir
 
     import pytorch_lightning as pl
+    from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 
     #from lightning.pytorch.loggers.wandb import WandbLogger
-    from pytorch_lightning import Trainer
-    from pytorch_lightning.loggers import CometLogger
+    #from pytorch_lightning import Trainer
+    #from pytorch_lightning.loggers import CometLogger
 
     # UPDATE / hack : Files now copied in bash command line before launching script
     # Copy timeseries and extracted features .h5 files locally onto slurm (compute node local scratch)
@@ -75,8 +79,12 @@ def train(config: DictConfig) -> None:
     #    ),
     #)
     # try instead: https://www.comet.com/site/
-    #https://lightning.ai/docs/pytorch/stable/extensions/generated/lightning.pytorch.loggers.CometLogger.html
-    # TODO: figure out comet API KEY
+    #
+    # Comet quick quide
+    # https://www.comet.com/mariestlaurent/quick-start?fromGetStarted=true
+    #
+    # https://lightning.ai/docs/pytorch/stable/extensions/generated/lightning.pytorch.loggers.CometLogger.html
+    # TODO: figure out comet API KEY / offline mode (save local dir)
     #logger = CometLogger(
     #    api_key=os.environ.get("COMET_API_KEY"),
     #    workspace=os.environ.get("COMET_WORKSPACE"),  # Optional
@@ -87,6 +95,30 @@ def train(config: DictConfig) -> None:
     #    experiment_name="lightning_logs",  # Optional
     #)
 
+    """
+    Adapted from: https://github.com/courtois-neuromod/video_transformer/blob/0906e9a71a2fdb511190f7a757c8aadcb1f6c990/scripts/train_gpt_ba.py#L33
+    """
+    callbacks = [
+        ModelCheckpoint(
+            monitor="val/brain_loss",
+            filename="last_brainenc",
+            mode="min",
+            save_last=True,
+        ),
+        ModelCheckpoint(
+            monitor="val/brain_loss",
+            filename="best_brainenc_{epoch}-{step}",
+            mode="min",
+            save_last=False,
+        ),
+        LearningRateMonitor(logging_interval="epoch"),
+    ]
+
+    logger = instantiate(
+        config.logger,
+        save_dir=f"{os.getcwd()}/logs",
+    )
+
     # instantiates datamodule config within datamodule class from config params
     datamodule = instantiate(
         config.datamodule,
@@ -96,19 +128,30 @@ def train(config: DictConfig) -> None:
         config.litmodule,
     )
 
-    trainer = Trainer(
-        logger=[logger],
-         **config.trainer,
+    trainer = instantiate(
+        config.trainer,
+        logger=logger,
+        callbacks=callbacks,
     )
 
-    #trainer.fit(model=litmodule, datamodule=datamodule)
+    trainer.fit(model=litmodule, datamodule=datamodule)
 
+    logging.info(
+        f"Best model saved at {callbacks[0].best_model_path}, \
+           with a val loss of {callbacks[0].best_model_score}"
+    )
     #trainer.save_checkpoint(config.output_dir)
+
+    # TODO: save checkpoints?... trainer params?
+    # TODO: implement LoRA
+    # TODO: Comet: how to log, save, view results (offline mode)
+    # https://www.comet.com/mariestlaurent/quick-start?fromGetStarted=true
+    # https://lightning.ai/docs/pytorch/stable/extensions/generated/lightning.pytorch.loggers.CometLogger.html
 
     #return trainer.validate(
     #    model=litmodule,
     #    datamodule=datamodule,
-    #)[0]["val/loss"]
+    #)[0]["val/brain_loss"]
 
 
 if __name__ == "__main__":
