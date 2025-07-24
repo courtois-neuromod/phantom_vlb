8166520a436b1b887226427428683ca4815232da
MarieStLaurent <marie.stl@gmail.com>
2025-07-24 11:36:27 -0400

    padding params to cpu

--- src/just_torch/litmodule_copy.py
+++ src/just_torch/litmodule_copy.py
@@ -238,7 +238,7 @@ class VLBLitModule(LightningModule):
         #attention_mask = x_lang.ne(0).long().to(self.device)
 
         weight_mask = make_weight_mask(
-            batch["padvals"].numpy(),
+            batch["padvals"].cpu().numpy(),
             batch["vis_weights"]*3,
             batch["lang_weights"]*3,
             x_lang.shape[1],
@@ -294,7 +294,7 @@ class VLBLitModule(LightningModule):
         #attention_mask = x_lang.ne(0).long().to(self.device)
 
         weight_mask = make_weight_mask(
-            batch["padvals"].numpy(),
+            batch["padvals"].cpu().numpy(),
             batch["vis_weights"],
             batch["lang_weights"],
             x_lang.shape[1],
